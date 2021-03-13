5c86ce725f990926f006559130ccc80b165f974e
drygig <natalia.drygas06@gmail.com>
2021-03-13 21:34:55 +0100

    bot.py added

---
+++ Simple Chatty Bot/Simple Chatty Bot/task/bot/bot.py
@@ -0,0 +1,57 @@
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
+
+
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
+
+
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    # write your code here
+
+    print("From where Python name is taken?")
+    print("1. snake")
+    print("2. Monty Python")
+    print("3. revolver")
+    print("4. all answers are correct")
+    answer = int(input("What is your answer?"))
+    if answer == 2:
+        print('Completed, have a nice day!')
+    else:
+        print("Please, try again.")
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
